a41938492e5511f8ef99f14511112d2feffb0178
HyeongJun-Noh <munhwas1140@naver.com>
2022-09-26 17:21:42 +0900

    2022-09-26

--- parc.cpp
+++ parc.cpp
@@ -2,36 +2,49 @@
 using namespace std;
 #define fastio cin.tie(0)->sync_with_stdio(0)
 using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+
+vector<double> a, b;
+int n;
+double f(double m) {
+    double ret = -1e9;
+    for(int i = 0; i < n; i++) {
+        ret = max(ret, b[i] + abs(m - a[i]));
+    }
+    return ret;
+}
 int main() {
     fastio;
     int tc; cin >> tc;
     while(tc--) {
-        int n, k; cin >> n >> k;
-        vector<ll> a(n + 2);
-        for(int i = 1; i <= n; i++) cin >> a[i];
-        int l = k;
-        int r = k;
-        ll now = a[k];
-        while(true) {
-            if(a[l - 1] > a[r + 1]) {
-                now += a[l - 1];
-                l -= 1;
-            } else if(a[r + 1] > a[l - 1]) {
-                now += a[r + 1];
-                r += 1;
-            } else {
-                
-            }
-            if(l == 0 || r == n + 1 || now < 0) break;
+        a.clear();
+        b.clear();
+        cin >> n;
+        double l = 1e9, r = -1e9;
+        a.resize(n); b.resize(n);
+        for(int i = 0; i < n; i++) {
+            cin >> a[i];
+            l = min(l, a[i]);
+            r = max(r, a[i]);
         }
-        
-        if(l == 0 || r == n + 1) {
-            cout << "YES" << '\n';
-        } else {
-            cout << "NO" << '\n';
+        for(int j = 0; j < n; j++) {
+            cin >> b[j];
         }
-        
+
+        double s = l;
+        double e = r;
+        while(e - s >= 0.0000001) {
+            double p = (s * 2 + e) / 3.0;
+            double q = (s + e * 2) / 3.0;
+            if(f(p) <= f(q)) e = q;
+            else s = p;
+        }
+        cout << fixed << setprecision(6);
+        cout << s << '\n';
     }
- 
     return 0;
 }
--- ppp.cpp
+++ ppp.cpp
@@ -7,36 +7,45 @@ using pii = pair<int, int>;
 using pll = pair<ll, ll>;
 using pil = pair<int, ll>;
 using pli = pair<ll, int>;
-
+ll ans = 0;
+vector<int> a;
+vector<pair<int,int>> tree;
+void init() {
+    a.clear();
+    tree.clear();
+    ans = 0;
+}
+void init(int node, int s, int e) {
+    if(s == e) {
+        tree[node] = make_pair(a[s], a[e]);
+        ans += 1LL;
+        return ;
+    } else {
+        int m = (s + e) / 2;
+        init(node * 2, s, m);
+        init(node * 2 + 1, m + 1, e);
+        tree[node].first = min(tree[node*2].first, tree[node * 2 + 1].first);
+        tree[node].second = max(tree[node * 2].second, tree[node * 2 + 1].second);
+        if(tree[node].second % tree[node].first == 0) {
+            ans += 1LL;
+        }
+    }
+}
 int main() {
     fastio;
     int tc; cin >> tc;
     while(tc--) {
+        init();
         int n; cin >> n;
-        vector<int> a(n);
-        vector<pair<int, int>> seq;
-        for(int i = 0; i < n; i++) {
+        a.resize(n + 1);
+        int h = (int)ceil(log2(n));
+        tree.resize(1 << (h + 1));
+        for(int i = 1; i <= n; i++) {
             cin >> a[i];
-            if(i > 0) {
-                if(a[i] < a[i-1]) {
-                    for(int j = 0; j < i - 1; j++) {
-                        if(a[i-j] > a[i-j-1]) break;
-                        seq.push_back({i-j, i-j+1});
-                        if((a[i-j] + a[i-j-1]) % 2 == 1) {
-                            a[i-j] = a[i-j-1];
-                            break;
-                        }
-                        a[i-j] = a[i-j-1];
-                    }
-                }
-            }
         }
-        cout << seq.size() << '\n';
-        for(auto &p : seq) {
-            cout << p.first << ' ' << p.second << '\n';
-        }
+        init(1,1,n);
+        cout << ans << '\n';
 
     }
-
     return 0;
 }
