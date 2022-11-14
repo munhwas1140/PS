7e04da5246b5818b215c7761def0729748570cdf
HyeongJun-Noh <munhwas1140@naver.com>
2022-11-14 23:03:48 +0900

    2022-11-14

---
+++ BOJ_C++/1748_d.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+void solve() {
+    ll a, b, d;
+    cin >> a >> b >> d;
+    cout << a << b << ' ' << d << '\n';
+}
+int main() {
+    fastio;
+    int tc; cin >> tc;
+    while(tc--) {
+        solve();
+    }
+    return 0;
+}
