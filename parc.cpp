9267739a1cd219844dfcc34d46d0bd60d1baabe2
HyeongJun-Noh <munhwas1140@naver.com>
2022-11-28 19:34:09 +0900

    2022-11-28

--- parc.cpp
+++ parc.cpp
@@ -5,50 +5,7 @@ using ll = long long;
 using pii = pair<int, int>;
 using pll = pair<ll, ll>;
 void solve() {
-    int n, x;
-    cin >> n >> x;
-    vector<int> a(n + 1);
-    a[1] = x;
-    a[n] = 1;
 
-    if(x == n) {
-        cout << n << ' ';
-        for(int i = 2; i < n; i++) {
-            cout << i << ' ';
-        }
-        cout << 1 << '\n';
-        return ;
-    }
-
-    if(n % x != 0) {
-        cout << -1 << '\n';
-        return ;
-    } else {
-        vector<int> tt;
-        for(int i = 2; i < n; i++) {
-            a[i] = i;
-        }
-        
-        int tmp = n / x;
-        int t2 = tmp;
-        for(int i = 2; i <= tmp; i++) {
-            while(t2 % i == 0) {
-                tt.push_back(i);
-                t2 /= i;
-            }
-        }
- 
-        int sz = tt.size();
-        t2 = n;
-        for(int i = sz - 1; i >= 0; i--) {
-            a[t2 / tt[i]] = t2;
-            t2 /= tt[i];
-        }
-    }
-    for(int i = 1; i <= n; i++) {
-        cout << a[i] << ' ';
-    }
-    cout << '\n';
 }
 int main() {
     fastio;
--- ppp.cpp
+++ ppp.cpp
@@ -5,27 +5,6 @@ using ll = long long;
 using pii = pair<int, int>;
 using pll = pair<ll, ll>;
 void solve() {
-    int n; cin >> n;
-    if(n == 2) {
-        cout << 3 << ' ' << 1 << '\n';
-        return ;
-    }
-    
-
-    
-    for(ll s = 1; ;s++) {
-
-        for(ll e = s + n - 1; ;e++) {
-            ll b = (e * (e + 1)) / 2;
-            ll a = ((e - n + 1) * (e - n + 2)) / 2;
-            ll x = b - a + s;
-            ll sq = (e - s) * (e - s);
-            if(x >= sq) {
-
-            }
-
-        }
-    }
 
 }
 int main() {
