2f6cddc7cf77e905b427016e0dd356bbe1d246f4
HyeongJun-Noh <munhwas1140@naver.com>
2022-12-02 16:50:57 +0900

    2022-12-02

--- BOJ_C++/15927.cpp
+++ BOJ_C++/15927.cpp
@@ -4,30 +4,27 @@ using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
 using pll = pair<ll, ll>;
-int m[26];
-int M[26];
-int a[500000];
 int main() {
     fastio;
     string str; cin >> str;
-    int n = str.size();
-    memset(m, -1,sizeof(m));
-
-    for(int i = 0; i < n; i++) {
-        int now = str[i] - 'A';
-        a[i] = now;
-        if(m[now] == -1) m[now] = i;
-        M[now] = max(M[now], i);
-    }
-
-    int ans = -1;
-    for(int i = 0; i < 26; i++) {
-        for(int j = 0; j < 26; j++) {
-            if(i == j || m[i] == -1 || m[j] == -1 || M[j] < m[i]) continue;
-            ans = max(ans, M[j] - m[i] + 1);
+    int s = 0;
+    int e = str.size() - 1;
+    while(s < e) {
+        if(str[s] != str[e]) {
+            return !(cout << (int)str.size() << '\n');
         }
+        s++;
+        e--;
     }
-    cout << ans << '\n';
-    ABCABCACBACBA
+    s = 0;
+    e = str.size() - 2;
+    while(s < e) {
+        if(str[s] != str[e]) {
+            return !(cout << (int)str.size() - 1 << '\n');
+        }
+        s++;
+        e--;
+    }
+    cout << -1 << '\n';
     return 0;
 }
