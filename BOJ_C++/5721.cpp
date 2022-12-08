2cb1abc0295c7d29b35e55bf70b43f0a8e69f10f
HyeongJun-Noh <munhwas1140@naver.com>
2022-12-09 01:22:15 +0900

    2022-12-09

--- BOJ_C++/5721.cpp
+++ BOJ_C++/5721.cpp
@@ -5,23 +5,50 @@ using ll = long long;
 using pii = pair<int, int>;
 using pll = pair<ll, ll>;
 int n, m;
+vector<vector<int>> dp, a;
+int go(int x, int y) {
+    int &ret = dp[x][y];
+    if(ret != -1) return ret;
+    ret = a[x][y];
+    
+    for(int next = y + 2; next < min(m, y + 4); next++) {
+        ret = max(ret, go(x, next) + a[x][y]);
+    }
+
+    for(int next = x + 2; next < min(n, x + 4); next++) {
+        ret = max(ret, go(next, 0) + a[x][y]);
+        if(m >= 1) {
+            ret = max(ret, go(next, 1) + a[x][y]);
+        }
+    }
+    return ret;
+}
 void solve() {
-    cin >> n >> m;
-    if(n == 0 && m == 0) return ;
-    vector<vector<int>> a(n, vector<int>(m, 0));
+    dp = vector<vector<int>>(n, vector<int>(m, -1));
+    a = vector<vector<int>>(n, vector<int>(m, 0));
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
             cin >> a[i][j];
         }
     }
-
-
     
+    int ans = -1;
+    for(int i = 0; i < min(n, 2); i++) {
+        ans = max(ans, go(i,0));
+        if(m >= 2) {
+            ans = max(ans, go(i, 1));
+        }
+    }
+
+    cout << ans << '\n';
 }
 int main() {
     fastio;
     while(true) {
+        cin >> n >> m;
+        if(!n && !m) break;
         solve();
     }
+
     return 0;
 }
