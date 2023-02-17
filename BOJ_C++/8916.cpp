33f8a70bffa2056d885022a2e8f46335c1939965
Hyeong Jun Noh <munhwas1140@naver.com>
2023-02-17 21:48:04 +0900

    2023-02-17

--- BOJ_C++/8916.cpp
+++ BOJ_C++/8916.cpp
@@ -7,9 +7,10 @@ using pll = pair<ll, ll>;
 const int MOD = 9999991;
 struct node {
     node *l, *r;
-    int v, sz = 0;
+    int v;
     node () : l(nullptr), r(nullptr) {}
 };
+ll f[21];
 
 node *insert(node *tree, int val) {
     if(tree == nullptr) {
@@ -26,8 +27,10 @@ node *insert(node *tree, int val) {
     return tree;
 }
 vector<int> g[21];
+int sz[21];
 void solve() {
     for(int i = 1; i <= 20; i++) g[i].clear();
+    memset(sz, 0, sizeof(sz));
     int n; cin >> n;
     node *tree = new node();
     for(int i = 0; i < n; i++) {
@@ -40,45 +43,35 @@ void solve() {
     }
 
     function<void(node *)> dfs = [&] (node *now) {
+        sz[now->v] = 1;
         if(now->l != nullptr) {
             g[now->v].push_back(now->l->v);
             dfs(now->l);
+            sz[now->v]++;
         }
         if(now->r != nullptr) {
             g[now->v].push_back(now->r->v);
             dfs(now->r);
+            sz[now->v]++;
         }
     };
     dfs(tree);
 
     vector<bool> check(21,false);
-    
-
     function<int(int, int)> go = [&] (int now, int idx) {
-        if(idx == n) return 1;
-        int ret = 0;
+        
+    }
 
-        for(int next : g[now]) {
-            check[next] = true;
-        }
 
-        for(int i = 1; i <= n; i++) {
-            if(check[i]) {
-                check[i] = false;
-                ret += go(i, idx + 1);
-                ret %= MOD;
-                check[i] = true;
-            }
-        }
-        for(int next : g[now]) {
-            check[next] = false;
-        }
-        return ret;
-    };
-    
     cout << go(tree->v, 1) << '\n';
 }
+
 int main() {
+    f[0] = 1;
+    f[1] = 1;
+    for(int i = 2; i <= 20; i++) {
+        f[i] = f[i - 1] * (ll)i;
+    }
     fastio;
     int tc; cin >> tc;
     while(tc--) {
