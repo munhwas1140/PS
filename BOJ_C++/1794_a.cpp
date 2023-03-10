b337d1a3180529ad1ae72c493cc539553f4290d7
Hyeong Jun Noh <munhwas1140@naver.com>
2023-03-10 14:23:15 +0900

    2023-03-10

--- BOJ_C++/1794_a.cpp
+++ BOJ_C++/1794_a.cpp
@@ -4,15 +4,12 @@ using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
 using pll = pair<ll, ll>;
-int dp[1000001];
 int main() {
     fastio;
-    int n; cin >> n;
-    vector<int> a(n);
-    for(int i = 0; i < n; i++) {
-        cin >> a[i];
-        dp[a[i]] = dp[a[i] - 1] + 1;
+    int tc; cin >> tc;
+    while(tc--) {
+        solve();
     }
-    cout << n - *max_element(dp, dp + 1000001) << '\n';
+
     return 0;
 }
