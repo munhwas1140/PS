13f4d24a7bad7244e2514dc96a343466a85a2b61
Hyeong Jun Noh <munhwas1140@naver.com>
2023-04-22 00:22:42 +0900

    2023-04-22

--- BOJ_C++/20302.cpp
+++ BOJ_C++/20302.cpp
@@ -17,44 +17,49 @@ int main() {
             }
         }
     }
-
     vector<int> sum(100001, 0);
+
     int tmp; cin >> tmp;
-    bool f = false;
-    if(tmp == 0) f = true;
-    while(f == false && tmp != 1) {
+    
+    bool zero = false;
+    if(tmp == 0) zero = true;
+    if(tmp < 0) tmp = -tmp;
+    while(zero == false && tmp != 1) {
         sum[p[tmp]]++;
         tmp /= p[tmp];
     }
+
     for(int i = 0; i < n - 1; i++) {
         char op;
         cin >> op >> tmp;
-        if(f == true) continue;
+
+        if(tmp == 0) zero = true;
+        if(zero == true) continue;
+
         if(tmp < 0) tmp = -tmp;
+
         if(op == '/') {
             while(tmp != 1) {
                 sum[p[tmp]]--;
                 tmp /= p[tmp];
             }
         } else {
-            if(tmp == 0) {
-                sum.clear();
-                f = true;
-                continue;
-            }
-            while(tmp != 1) {
+            while(zero == false && tmp != 1) {
                 sum[p[tmp]]++;
                 tmp /= p[tmp];
             }
         }
     }
-    if(f == true) return !(cout << "mint chocolate" << '\n');
-    for(int i = 2; i <= 100000; i++) {
-        if(sum[i] < 0) {
-            return !(cout << "toothpaste" << '\n');
+
+    if(zero == true) cout << "mint chocolate" << '\n';
+    else {
+        for(int i = 2; i <= 100000; i++) {
+            if(sum[i] < 0) {
+                return !(cout << "toothpaste" << '\n');
+            }
         }
+        cout << "mint chocolate" << '\n';
     }
-    cout << "mint chocolate" << '\n';
 
     return 0;
 }
