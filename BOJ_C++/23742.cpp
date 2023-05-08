c3ec449ca8230d78aacdfe6b13a51378dcc3db59
Hyeong Jun Noh <munhwas1140@naver.com>
2023-05-08 15:53:37 +0900

    2023-05-08

---
+++ BOJ_C++/23742.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+int main() {
+    fastio;
+    int n; cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) {
+        
+    }
+
+    return 0;
+}
