d3e4cc0f3bd3512b6f29b2034782c85dddc900ed
Hyeong Jun Noh <munhwas1140@naver.com>
2023-09-08 12:19:02 +0900

    2023-09-08

---
+++ BOJ_C++/1938.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+int n;
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+int ddx[] = {-1,-1,-1,0,0,1,1,1};
+int ddy[] = {-1,0,1,-1,1,-1,0,1};
+int main() {
+    fastio;
+    cin >> n;
+    vector<string> a(n);
+    for(int i = 0; i < n; i++) {
+        
+    }
+
+    return 0;
+}
