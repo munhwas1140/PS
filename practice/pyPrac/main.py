8d7e290cfb3b1d550d2910ff1c644da215b5b83d
Hyeong Jun Noh <munhwas1140@naver.com>
2023-12-12 01:16:53 +0900

    PS 폴더 변경

--- practice/pyPrac/main.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-import keyboard
-import time
-
-while 1:
-    position = pyautogui.position()
-    if keyboard.is_pressed('enter'):
-        print(position)
-        time.sleep(0.2)
