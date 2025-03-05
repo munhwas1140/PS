f93818274937abfa89ae9c62f643f2bbab255b70
munhwas1140 <munhwas1140@gmail.com>
2025-03-06 03:21:25 +0900

    add .clang-format, 22251.cppadd .clang-format, 22251.cppadd .clang-format, 22251.cppadd .clang-format, 22251.cppadd .clang-format, 22251.cppadd .clang-format, 22251.cppadd .clang-format, 22251.cppadd .clang-format, 22251.cppadd .clang-format, 22251.cppadd .clang-format, 22251.cppadd .clang-format, 22251.cppadd .clang-format, 22251.cppadd .clang-format, 22251.cppadd .clang-format, 22251.cppadd .clang-format, 22251.cppadd .clang-format, 22251.cppadd .clang-format, 22251.cppadd .clang-format, 22251.cppadd .clang-format, 22251.cppadd .clang-format, 22251.cppadd .clang-format, 22251.cppadd .clang-format, 22251.cpp

---
+++ C++/22251.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+int number[][7] = {{1, 1, 1, 0, 1, 1, 1}, {0, 0, 1, 0, 0, 1, 0},
+                   {1, 0, 1, 1, 1, 0, 1}, {1, 0, 1, 1, 0, 1, 1},
+                   {0, 1, 1, 1, 0, 1, 0}, {1, 1, 0, 1, 0, 1, 1},
+                   {1, 1, 0, 1, 1, 1, 1}, {1, 0, 1, 0, 0, 1, 0},
+                   {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 0, 1, 1}};
+
+int main() {
+    fastio;
+
+    int n, k, p, x;
+    cin >> n >> k >> p >> x;
+
+    return 0;
+}
